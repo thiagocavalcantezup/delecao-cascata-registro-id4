02a2f64a2468e2af4f55cc281ffd7e1dabda761c
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-13 23:57:20 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/delecaocascataregistroid4/DelecaoCascataRegistroId4Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.delecaocascataregistroid4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelecaoCascataRegistroId4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelecaoCascataRegistroId4Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/handora/delecaocascataregistroid4/DelecaoCascataRegistroId4ApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.handora.delecaocascataregistroid4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelecaoCascataRegistroId4ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
